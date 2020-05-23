aebe922c2cc4997fb773273c335a0c6426e376c2
kiss-gergely@outlook.com <ag0OO5xC8D66mwFo>
2020-05-23 15:56:57 +0100

    Basic test. Basic dependencies.

--- src/test/java/dev/shivamaha/apidemo/ApiDemoApplicationTests.java
+++ src/test/java/dev/shivamaha/apidemo/ApiDemoApplicationTests.java
@@ -1,13 +1,26 @@
 package dev.shivamaha.apidemo;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class ApiDemoApplicationTests {
+	private final ConfigurableApplicationContext context;
+
+	ApiDemoApplicationTests(ConfigurableApplicationContext context) {
+		this.context = context;
+	}
 
 	@Test
 	void contextLoads() {
+		Assert.assertNotNull(context);
 	}
+	@Test
+	void mainTest(){
+		ApiDemoApplication.main(new String[]{});
+	}
+
 
 }
